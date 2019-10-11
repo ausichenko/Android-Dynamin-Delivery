d690b147fede0e90dedc1817bdfabbf58605f056
Andrey Usichenko <usichenko.andrey@gmail.com>
2019-10-11 15:25:30 +0700

    Add test dynamic feature.

--- app/src/main/java/com/ausichenko/dynamicdeliverytest/MainActivity.kt
+++ app/src/main/java/com/ausichenko/dynamicdeliverytest/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.ausichenko.dynamicdeliverytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun routeToDynamic(view: View) {
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+
+        val request = SplitInstallRequest
+            .newBuilder()
+            .addModule("dynamicfeature")
+            .build()
+
+        splitInstallManager
+            .startInstall(request)
+            .addOnSuccessListener { sessionId ->
+                val c = Class.forName("com.ausichenko.dynamicfeature.DynamicActivity")
+                startActivity(Intent(this, c))
+            }
+            .addOnFailureListener { exception ->
+
+            }
     }
 }
---
+++ dynamicfeature/src/main/java/com/ausichenko/dynamicfeature/DynamicActivity.kt
@@ -0,0 +1,12 @@
+package com.ausichenko.dynamicfeature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DynamicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamic)
+    }
+}
